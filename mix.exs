82bac85941efe3ab19fc904308903219cd4ed742
Tymon Tobolski <i@teamon.eu>
2017-04-10 17:58:01 +0200

    Use twin from github

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Bunny.Mixfile do
 
       # dev & test
       {:mix_test_watch, "~> 0.2", only: :dev},
-      {:twin, path: "~/code/twin"},
+      {:twin, github: "recruitee/twin"},
     ]
   end
 end
